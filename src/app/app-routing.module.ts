82802314119410d8ed9e48635a41197680b7418e
Cristian Cajiao <cristian.cajiao@alumnos.usm.cl>
2021-11-06 08:35:33 -0300

    Implementados módulos de prueba

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
+  { path: 'one', loadChildren: () => import('./one/one.module').then(m => m.OneModule) },
+  { path: 'two', loadChildren: () => import('./two/two.module').then(m => m.TwoModule) },
+  { path: 'three', loadChildren: () => import('./three/three.module').then(m => m.ThreeModule) },
+  { path: '**', redirectTo: '/home', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
